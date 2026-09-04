81715d94a8ad1bbea7b0eee5424f2a237177bcfe
Lucas Lima <lucaslima13861@gamil.com>
2026-09-04 16:52:46 -0300

    Product-OrderItem one-to-many association

--- src/main/java/lucas/lima/dev/teste/spring/entites/Product.java
+++ src/main/java/lucas/lima/dev/teste/spring/entites/Product.java
@@ -1,5 +1,6 @@
 package lucas.lima.dev.teste.spring.entites;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -22,6 +23,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
 
@@ -76,6 +80,16 @@ public class Product implements Serializable {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+
+        }
+        return set;
     }
 
     @Override
